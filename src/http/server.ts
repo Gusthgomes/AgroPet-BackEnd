decaecf80d67ea1df324dd4143e99bafb2dba326
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-09-18 20:46:39 -0300

    feat: registering routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,25 @@
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import fastifyCors from "@fastify/cors";
 
-const app = fastify();
+import { createGoalRoute } from "../routes/category/create-category";
+
+const app = fastify({
+  logger: true,
+}).withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createGoalRoute);
 
 app
   .listen({
