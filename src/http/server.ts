d13d8a584ad2bf69551ddaae1178824697e84a55
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-09-17 21:01:01 -0300

    chore: add fastify

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log("Ola que tal");
+import fastify from "fastify";
+
+const app = fastify();
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running on port 3333");
+  });
