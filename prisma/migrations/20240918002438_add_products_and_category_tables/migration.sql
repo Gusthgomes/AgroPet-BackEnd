792093f4cc163c065b957bbc270f14b64f8a3322
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-09-17 21:38:16 -0300

    feat: running migrations

---
+++ prisma/migrations/20240918002438_add_products_and_category_tables/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrls" TEXT[],
+    "basePrice" DECIMAL(8,2) NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "discountPercentage" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
